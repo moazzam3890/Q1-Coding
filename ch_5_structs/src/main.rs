4e58cdcf631dad3a94415279dd56a4ad2f028922
Moazzam Adil Khan <moaadi3890@gmail.com>
2020-07-02 12:49:17 +0500

    Code for printing of each attribute through method has been added

--- ch_5_structs/src/main.rs
+++ ch_5_structs/src/main.rs
@@ -24,6 +24,22 @@ impl Person {
     fn printing_whole(&self){
         println!("Complete details are : {:#?}", self);
     }
+
+    fn id(&self){
+        println!("Your ID is {}", self.id);
+    }
+    fn first_name(&self){
+        println!("Your First name is {}", self.first_name);
+    }
+    fn last_name(&self){
+        println!("Your Last name is {}", self.last_name);
+    }
+    fn age(&self){
+        println!("Your Age is {}", self.age);
+    }
+    fn height(&self){
+        println!("Your Height is {}", self.height);
+    }
 }
 
 fn main () {
@@ -40,10 +56,12 @@ fn main () {
     let mut l_name_in = String::new();
     io::stdin().read_line(&mut l_name_in).expect("Please Enter a Valid Name");
 
+    println!("Please Enter your Age");
     let mut age_in = String::new();
     io::stdin().read_line(&mut age_in).expect("Please Enter a Valid Age");
     let age_in: u8 = age_in.trim().parse().expect("Please enter a Valid Age");
 
+    println!("Please Enter your Height");
     let mut height_in = String::new();
     io::stdin().read_line(&mut height_in).expect("Please Enter a Valid Height");
     let height_in: f32 = height_in.trim().parse().expect("Please enter a Valid Height");
@@ -54,4 +72,25 @@ fn main () {
     //Calling Method:
     person.printing_whole();
 
+    println!("Press 1 for ID, 2 for First Name, 3 for Last Name,
+    4 for age, 5 for Height");
+    let mut opt = String::new();
+    io::stdin().read_line(&mut opt).expect("Error");
+    let opt: u8 = opt.trim().parse().expect("Please enter a Valid Number");
+
+    if opt == 1 {
+        person.id();
+    }
+    else if opt == 2 {
+        person.first_name();
+    }
+    else if opt == 3 {
+        person.last_name();
+    }
+    else if opt == 4 {
+        person.age();
+    }
+    else {
+        person.height();
+    }
 }
